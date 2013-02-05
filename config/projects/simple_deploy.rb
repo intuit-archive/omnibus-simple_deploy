8555139ca1018d69ee8a5f9faaea07c2c572b422
Brett Weaver <brett@weav.net>
2013-02-05 14:28:20 -0800

    initial commit

---
+++ config/projects/simple_deploy.rb
@@ -0,0 +1,9 @@
+name            "simple_deploy"
+
+install_path    "/opt/intu/simple_deploy"
+build_version   "0.6.6"
+build_iteration "1"
+
+dependencies    ["preparation", "simple_deploy"]
+
+exclude         "\.git*"
---
+++ config/software/simple_deploy.rb
@@ -0,0 +1,10 @@
+name "simple_deploy"
+version "0.10.0"
+
+dependencies ["libxslt", "libxml2", "rubygems", "ruby"]
+
+env = { "GEM_HOME" => nil, "GEM_PATH" => nil }
+
+build do
+  gem "install simple_deploy -n #{install_dir}/bin --no-rdoc --no-ri -v #{version}", :env => env
+end
