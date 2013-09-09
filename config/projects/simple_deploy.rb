ee7ffe187d6b6d3fb599e39cbc44016cc8d87fea
Kevin Young <kevin_young@intuit.com>
2013-09-09 13:15:27 -0700

    updates

--- config/projects/simple_deploy.rb
+++ config/projects/simple_deploy.rb
@@ -1,4 +1,6 @@
 name            "simple_deploy"
+maintainer      "Intuit, Inc."
+homepage        "git@github.com:intuit/simple_deploy.git"
 
 install_path    "/opt/intu/simple_deploy"
 build_version   ENV['version']
